3e338445b470fd18f08ae53f598e181d37d93706
maricon <maricon@mariconcete.myguest.virtualbox.org>
2025-06-10 14:40:41 +0200

    visuales del menu

--- data/menu.js
+++ data/menu.js
@@ -60,3 +60,4 @@ const menu = [
         ]
     }   
 ]
+export default menu;
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,10 @@
+import { TIMEOUT } from "dns/promises";
 import express from "express";
+import menu from "../data/menu.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("home.njk", { titulo: "Home"});  
+    res.render("home.njk", { title: "Home", menu});  
 })
 
 export default router;
