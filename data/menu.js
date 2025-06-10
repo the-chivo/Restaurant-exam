dd04f27adc92d15056848ed75fa6c8e7986a4fc1
maricon <maricon@mariconcete.myguest.virtualbox.org>
2025-06-10 13:53:12 +0200

    menu añadido

---
+++ data/menu.js
@@ -0,0 +1,62 @@
+const menu = [
+    {
+        categoria: "rollo",
+        platos: [
+            {nombre: "rollo de pollo", descripcion: "Lechuga, tomate, maiz, cebolla, pollo", precio: 4.50 },
+            {nombre: "rollo de ternera", descripcion: "Lechuga, tomate, maiz, cebolla, ternera", precio: 5.50 },
+            {nombre: "rollo de cerdo", descripcion: "Lechuga, tomate, maiz, cebolla, cerdo", precio: 5.00 },
+            {nombre: "rollo mixto", descripcion: "Lechuga, tomate, maiz, cebolla, cerdo, pollo", precio: 5.00 },
+            {nombre: "rollo de falafel", descripcion: "Lechuga, tomate, maiz, cebolla, falafel", precio: 3.50 },
+        ]
+    },
+    {
+        categoria: "Pita",
+        platos: [
+            {nombre: "Pita de pollo", descripcion: "Lechuga, tomate, maiz, cebolla, pollo", precio: 4.00 },
+            {nombre: "Pita de ternera", descripcion: "Lechuga, tomate, maiz, cebolla, ternera", precio: 4.50 },
+            {nombre: "Pita de cerdo", descripcion: "Lechuga, tomate, maiz, cebolla, cerdo", precio: 4.00 },
+            {nombre: "Pita mixta", descripcion: "Lechuga, tomate, maiz, cebolla, cerdo, pollo", precio: 4.50 },
+            {nombre: "Pita de falafel", descripcion: "Lechuga, tomate, maiz, cebolla, falafel", precio: 3.00 },
+        ]
+    },
+    {
+        categoria: "Menu kebab",
+        platos: [
+            {nombre: "Menu de pollo", descripcion: "Rollo de pollo, bebida y patatas fritas", precio: 6.50 },
+            {nombre: "Menu de ternera", descripcion: "Rollo de ternera, bebida y patatas fritas", precio: 7.50 },
+            {nombre: "Menu de cerdo", descripcion: "Rollo de cerdo, bebida y patatas fritas", precio: 7.00 },
+            {nombre: "Menu mixto", descripcion: "Rollo mixto, bebida y patatas fritas", precio: 7.00 },
+            {nombre: "Menu de falafel", descripcion: "Rollo de falafel, bebida y patatas frityas", precio: 5.50 },
+        ]
+    },
+    {
+        categoria: "Pizzas",
+        platos: [
+            {nombre: "Pizza jamon york y queso", descripcion: "Tomate, queso, jamon york", precio: 6.00 },
+            {nombre: "Pizza cuatro quesos", descripcion: "Tomate, queso, queso azul, queso de cabra, queso mozzarella", precio: 7.00 },
+            {nombre: "Pizza barbacoa", descripcion: "Tomate, queso, pollo, cebolla, salsa barbacoa", precio: 7.50 },
+            {nombre: "Pizza vegetariana", descripcion: "Tomate, queso, pimiento, cebolla, champiñones", precio: 6.50 },
+            {nombre: "Pizza margarita", descripcion: "Tomate, queso, albahaca", precio: 5.50 },
+        ]
+    },
+    {
+        categoria: "Menu pizza",
+        platos: [
+            {nombre: "Menu pizza jamon york y queso", descripcion: "Pizza jamon york y queso, bebida y patatas fritas", precio: 8.00 },
+            {nombre: "Menu pizza cuatro quesos", descripcion: "Pizza cuatro quesos, bebida y patatas fritas", precio: 9.00 },
+            {nombre: "Menu pizza barbacoa", descripcion: "Pizza barbacoa, bebida y patatas fritas", precio: 9.50 },
+            {nombre: "Menu pizza vegetariana", descripcion: "Pizza vegetariana, bebida y patatas fritas", precio: 8.50 },
+            {nombre: "Menu pizza margarita", descripcion: "Pizza margarita, bebida y patatas fritas", precio: 7.50 },
+        ]
+    },
+    {
+        categoria: "Bebidas",
+        platos: [
+            {nombre: "Coca Cola", precio: 1.50 },
+            {nombre: "Fanta Naranja", precio: 1.50 },
+            {nombre: "Agua", precio: 1.00 },
+            {nombre: "Cerveza", precio: 2.00 },
+            {nombre: "tinto de verano", precio: 2.50 },
+        ]
+    }   
+]
